178aa463c6c9955a2672df036df805c6a66496b3
afgarnicar <afgarnicar@eafit.edu.co>
2026-02-01 13:00:45 -0500

    se agrego base y home .html, un static css y funcion en views

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse("Hello, World!")
-
+class homePageView(TemplateView):
+    template_name = 'home.html'
