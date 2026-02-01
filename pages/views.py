8a6a408994bb97b8d68ebdb10e44273eedbc92ba
afgarnicar <afgarnicar@eafit.edu.co>
2026-02-01 17:27:26 -0500

    Agregue el template de about.html

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from matplotlib.style import context
 
 # Create your views here.
 class homePageView(TemplateView):
     template_name = 'pages/home.html'
+    
+class AboutPageView(TemplateView): 
+    template_name = 'pages/about.html' 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+            })
+        return context
+
