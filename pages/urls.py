5456b9d99d6f640879b6ae6582911a358248ff87
afgarnicar <afgarnicar@eafit.edu.co>
2026-02-03 20:08:29 -0500

    Agregue template para ver lista de ciertos productos

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import homePageView, AboutPageView
+from .views import ProductIndexView, ProductShowView, homePageView, AboutPageView
 
 urlpatterns = [
     path("", homePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from matplotlib.style import context
+from django.views import View
+from django import forms
+
 
 # Create your views here.
 class homePageView(TemplateView):
@@ -15,7 +18,36 @@ class AboutPageView(TemplateView):
             "title": "About us - Online Store",
             "subtitle": "About us",
             "description": "This is an about page ...",
-            "author": "Developed by: Your Name",
+            "author": "Developed by: Andrés Garnica",
             })
         return context
 
+class Product: 
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV"}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"} ]
+    
+class ProductIndexView(View):
+    template_name = 'pages/products/index.html'
+    
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+        
+        return render(request, self.template_name, viewData)
+    
+class ProductShowView(View): 
+    template_name = 'pages/products/show.html'
+
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+        return render(request, self.template_name, viewData)
